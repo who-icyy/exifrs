b9776c4ce5e58e9f0e378cb04554c09187a24437
who-icyy <sonusid1325@gmail.com>
2024-10-06 20:24:38 +0530

    Initial

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+use std::path;
+
 use clap::{Arg, Command};
 mod read;
 fn main() {
     let matches = Command::new("<File location>")
-    .version("1.0.0")
-    .author("@Who-icyy")
-    .about("Exiftool clone but in RUST Programming.")
-    .arg(
-        Arg::new("path")
-        .help("<Target file path>")
-        .required(true)
-        .index(1)
-    ).get_matches();
+        .version("1.0.0")
+        .author("@Who-icyy")
+        .about("Exiftool clone but in RUST Programming.")
+        .arg(
+            Arg::new("path")
+                .help("<Target file path>")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new("write")
+                .short('w')
+                .long("write")
+                .help("<Write Metadata to file.>")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .get_matches();
 
-    let input: &String = matches.get_one::<String>("path").unwrap();
+    let file_path: &String = matches.get_one::<String>("path").unwrap();
 
-    read::exifextract(input);
 }
--- src/read.rs
+++ src/read.rs
@@ -19,35 +19,34 @@ pub fn exifextract(path: &String) {
             let attributes = meta.file_attributes();
             println!("File attributes: {}", attributes);
 
-            // Volume serial number (Windows-specific)
-            // let volume_serial_number = meta.volume_serial_number();
-            // println!("Volume serial number: {:?}", volume_serial_number);
-
-            // File index (Windows-specific)
-            // let file_index = meta.file_index();
-            // println!("File index: {:?}", file_index);
-
-            // Last access time
             if let Ok(access_time) = meta.accessed() {
                 if let Ok(duration) = access_time.duration_since(UNIX_EPOCH) {
-                    println!("Last accessed: {} seconds since UNIX EPOCH", duration.as_secs());
+                    println!(
+                        "Last accessed: {} seconds since UNIX EPOCH",
+                        duration.as_secs()
+                    );
                 }
             }
 
             // Creation time
             if let Ok(creation_time) = meta.created() {
                 if let Ok(duration) = creation_time.duration_since(UNIX_EPOCH) {
-                    println!("Creation time: {} seconds since UNIX EPOCH", duration.as_secs());
+                    println!(
+                        "Creation time: {} seconds since UNIX EPOCH",
+                        duration.as_secs()
+                    );
                 }
             }
 
-            // Last modified time
             if let Ok(modified_time) = meta.modified() {
                 if let Ok(duration) = modified_time.duration_since(UNIX_EPOCH) {
-                    println!("Last modified: {} seconds since UNIX EPOCH", duration.as_secs());
+                    println!(
+                        "Last modified: {} seconds since UNIX EPOCH",
+                        duration.as_secs()
+                    );
                 }
             }
-        },
+        }
         Err(e) => {
             println!("Error retrieving metadata: {}", e);
         }
