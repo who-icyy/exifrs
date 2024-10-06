dd3f0bd58d592cac663f79e2b06471c06465d0d5
who-icyy <sonusid1325@gmail.com>
2024-10-06 18:05:51 +0530

    Initi

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::{Arg, Command};
+
+fn main() {
+    let matches = Command::new("<File location>")
+    .version("1.0.0")
+    .author("@Who-icyy")
+    .about("Exiftool clone but in RUST Programming.")
+    .arg(
+        Arg::new("path")
+        .help("<Target file path>")
+        .required(true)
+        .index(1)
+    ).get_matches();
+
+    let input: &String = matches.get_one::<String>("path").unwrap();
+
+    println!("{}",input);
+}
